efa46690130ac448affec6a0e01c0c243f35178b
cyberbrain <cyber.brain.mfkg@gmail.com>
2016-01-18 20:20:49 +0100

    Dodat Sympy.

---
+++ sympy/example1.py
@@ -0,0 +1,12 @@
+#Osnovni primer u scypy-u
+#Scypy se koristi za simbolichko programiranje matematickih izraza.
+#Sto znaci da vrednosti ne moraju biti definisane pre kompjutacije,
+#vec ce se primenjivati matematicke operacije nad definisanim izrazima,
+#a uz pomoc supstitucije se menjaju vrednosti (u nove simbole ili konkretne vrednosti).
+
+# Cilj ove vezbe je da pokaze razliku izmedju simbolickog(scypy modul) i nesimbolickog programiranja (math modul).
+
+import scypy            # importuje scypy modul
+import math             # importyuje math modul
+
+
